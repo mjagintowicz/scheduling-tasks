f6d9a12b2e6dceb4d123c52ef2c56f52135dd0d4
Maria Jagintowicz <mjagintowicz1@gmail.com>
2024-10-22 18:03:26 +0200

    zmiany_w_gui

--- calendar.py
+++ calendar.py
@@ -1,1 +1,8 @@
 # funkcje realizujące łączenie z kalendarzem
+
+# POBIERANIE ZADAŃ Z KALENDARZA
+def get_tasks_from_calendar():
+
+    tasks_obtained = True       # flaga czy lista została pobrana
+
+    return tasks_obtained
--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QHBoxLayout, QWidget, QDateEdit, QVBoxLayout, QLabel, QTimeEdit
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QHBoxLayout, QWidget, QDateEdit, QVBoxLayout, QLabel, QTimeEdit, QTabWidget
 from PyQt6.QtCore import Qt, QDate, QTime
 from PyQt6.QtGui import QFont
 
-# OKNO STARTOWE
+from calendar import get_tasks_from_calendar
 
+
+# OKNO STARTOWE
 
 class StartWindow(QMainWindow):
 
@@ -17,72 +19,134 @@ class StartWindow(QMainWindow):
         # rozmiar okna
         self.setFixedSize(1500, 850)
 
-        # centralny widget
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        # layout główny
+        self.tab_layout = QVBoxLayout()
+
+        # zakładki
+        self.tabs = QTabWidget()
+        self.tabs.resize(300, 200)
+        self.setCentralWidget(self.tabs)
+        self.show()
+
+        # zakładka 1. - wczytywanie danych
+        self.tab1 = GetDataTab()
+        self.tabs.addTab(self.tab1, "Wczytaj zadania")
+
+        # zakładka 2. - dodawanie nowych zadań do kalendarza
+        self.tab2 = AddDataTab()
+        self.tabs.addTab(self.tab2, "Dodaj zadania")
+
+
+# WCZYTYWANIE DANYCH
+
+class GetDataTab(QWidget):
+
+    def __init__(self):
+
+        super(QWidget, self).__init__()
 
         # przycisk startowy
-        start_button = QPushButton('Pobierz listę zadań\nz kalendarza', self)
-        start_button.setFixedSize(400, 200)
-        start_button.setFont(QFont('Calibri', 25))
+        self.start_button = QPushButton('Pobierz zadania\nz kalendarza', self)
+        self.start_button.setFixedSize(400, 200)
+        self.start_button.setFont(QFont('Calibri', 25))
+        self.start_button.clicked.connect(self.get_data)    # po kliknięciu funkcja
+
+        self.tasks_obtained = False     # flaga
 
         # wybór dat
-        begin_date = QDateEdit()
-        begin_date.setCalendarPopup(True)
-        begin_date.setDate(QDate.currentDate())     # domyślnie dziś
-        begin_date.setFixedSize(100, 50)
+        self.begin_date = QDateEdit()
+        self.begin_date.setCalendarPopup(True)
+        self.begin_date.setDate(QDate.currentDate())  # domyślnie dziś
+        self.begin_date.setFixedSize(100, 30)
 
-        finish_date = QDateEdit()
-        finish_date.setCalendarPopup(True)
-        finish_date.setDate(QDate.currentDate().addDays(14))        # domyślnie koniec po 2 tygodniach
-        finish_date.setFixedSize(100, 50)
+        self.finish_date = QDateEdit()
+        self.finish_date.setCalendarPopup(True)
+        self.finish_date.setDate(QDate.currentDate().addDays(14))  # domyślnie koniec po 2 tygodniach
+        self.finish_date.setFixedSize(100, 30)
 
         # wybór godzin
-        begin_time = QTimeEdit()
-        begin_time.setTime(QTime.currentTime())
-        begin_time.setFixedSize(100, 50)
+        self.begin_time = QTimeEdit()
+        self.begin_time.setTime(QTime.currentTime())
+        self.begin_time.setFixedSize(80, 30)
 
-        finish_time = QTimeEdit()
-        finish_time.setTime(QTime.currentTime())
-        finish_time.setFixedSize(100, 50)
+        self.finish_time = QTimeEdit()
+        self.finish_time.setTime(QTime.currentTime())
+        self.finish_time.setFixedSize(80, 30)
 
         # etykiety
-        begin_label = QLabel('Początek harmonogramu:')
-        begin_label.setFixedSize(200, 20)
+        self.begin_label = QLabel('Początek harmonogramu:')
+        self.begin_label.setFixedSize(200, 20)
 
-        finish_label = QLabel('Koniec harmonogramu:')
-        finish_label.setFixedSize(200, 20)
+        self.finish_label = QLabel('Koniec harmonogramu:')
+        self.finish_label.setFixedSize(200, 20)
 
         # layout na informacje o dacie i godzinie
-        full_date_time_layout = QVBoxLayout()
+        self.full_date_time_layout = QVBoxLayout()
 
         # w nim mniejsze layouty
         # 1. - etykieta
-        begin_label_layout = QHBoxLayout()
-        begin_label_layout.addWidget(begin_label)
+        self.begin_label_layout = QHBoxLayout()
+        self.begin_label_layout.addWidget(self.begin_label)
 
         # 2. - data i godzina początku
-        begin_date_time_layout = QHBoxLayout()
-        begin_date_time_layout.addWidget(begin_date)
-        begin_date_time_layout.addWidget(begin_time)
+        self.begin_date_time_layout = QHBoxLayout()
+        self.begin_date_time_layout.addWidget(self.begin_date)
+        self.begin_date_time_layout.addWidget(self.begin_time)
 
         # 3. - etykieta
-        finish_label_layout = QHBoxLayout()
-        finish_label_layout.addWidget(finish_label)
+        self.finish_label_layout = QHBoxLayout()
+        self.finish_label_layout.addWidget(self.finish_label)
 
         # 4. - data i godzina końca
-        finish_date_time_layout = QHBoxLayout()
-        finish_date_time_layout.addWidget(finish_date)
-        finish_date_time_layout.addWidget(finish_time)
+        self.finish_date_time_layout = QHBoxLayout()
+        self.finish_date_time_layout.addWidget(self.finish_date)
+        self.finish_date_time_layout.addWidget(self.finish_time)
 
-        full_date_time_layout.addLayout(begin_label_layout)
-        full_date_time_layout.addLayout(begin_date_time_layout)
-        full_date_time_layout.addLayout(finish_label_layout)
-        full_date_time_layout.addLayout(finish_date_time_layout)
-        full_date_time_layout.setContentsMargins(0, 320, 0, 320)
+        self.full_date_time_layout.addLayout(self.begin_label_layout)
+        self.full_date_time_layout.addLayout(self.begin_date_time_layout)
+        self.full_date_time_layout.addLayout(self.finish_label_layout)
+        self.full_date_time_layout.addLayout(self.finish_date_time_layout)
+        self.full_date_time_layout.setContentsMargins(0, 320, 0, 320)
 
         # layout główny
-        start_layout = QHBoxLayout()
-        start_layout.addWidget(start_button, alignment=Qt.AlignmentFlag.AlignCenter)
-        start_layout.addLayout(full_date_time_layout)
-        central_widget.setLayout(start_layout)
+        self.start_layout = QHBoxLayout()
+        self.start_layout.addWidget(self.start_button, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.start_layout.addLayout(self.full_date_time_layout)
+        self.setLayout(self.start_layout)
+
+    # funkcja pobierająca dane
+    def get_data(self):
+
+        if self.begin_date.date() > self.finish_date.date() or \
+                (self.begin_date.date() == self.finish_date.date() and
+                 self.begin_time.date() >= self.finish_time.date()):
+            print("error")                      # jeśli podany jest zły przedział czasowy - komunikat o błędzie
+
+        else:
+            self.tasks_obtained = get_tasks_from_calendar()
+            # w przeciwnym wypadku wywoływanie właściwej funkcji pobierającej dane
+
+
+class AddDataTab(QWidget):
+
+    def __init__(self):
+
+        super(QWidget, self).__init__()
+
+        # layout główny
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+
+        # 1. utworzenie manualnie całkiem nowego setu zadań
+        self.layout_1 = QHBoxLayout()
+        self.layout.addLayout(self.layout_1)
+
+        self.label_1 = QLabel("Utwórz nową listę zadań")
+        self.layout_1.addWidget(self.label_1)
+
+        # 2. dodanie manulanie zadań do pobranego setu
+        self.layout_2 = QHBoxLayout()
+        self.layout.addLayout(self.layout_2)
+
+        self.label_2 = QLabel("Dodaj zadania do aktualnej listy")
+        self.layout_2.addWidget(self.label_2)
